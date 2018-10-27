49dac0e19315cc4b4bd0c970e001231e081b645f
howard <howarder3@gmail.com>
2018-10-28 03:05:33 +0800

    update sample

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ CHANNEL_ACCESS_TOKEN = "YQj3rgXGt7E3aZyHVWHnl/Q/+3JMLwlvrXCDwLhH+tniIj7T/r/5mWo6
 CHANNEL_SECRET = "1d169827c6fe5905f2c7b965cbfa5114"
 
 
+
 def user_guide():
     return TemplateSendMessage(
         alt_text='【今天想要使用什麼功能呢？】',
@@ -90,23 +91,40 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
-    if(user_message == "開始"):
+    if(user_message in ["開始","start"]):
         output_message = user_guide()
         line_bot_api.reply_message(event.reply_token, output_message)
     elif(user_message == "開始跑步"):
         output_message = TextSendMessage(text="已開始記錄！祝您跑步愉快！")  
         line_bot_api.reply_message(event.reply_token, output_message)
     elif(user_message == "結束跑步"):
-        output_message = TextSendMessage(text="好的！辛苦您了！\n以下是您的跑步結果：\n")  
+        output_message = TextSendMessage(text="好的！辛苦您了！\n以下是您的跑步結果：\ntest")  
         line_bot_api.reply_message(event.reply_token, output_message)
     elif(user_message == "天氣"):
-        output_message = TextSendMessage(text="以下是今天天氣供您參考：\n")  
+        output_message = TextSendMessage(text="以下是今天天氣供您參考：\ntest")  
         line_bot_api.reply_message(event.reply_token, output_message)
     elif(user_message == "出門注意事項"):
-        output_message = TextSendMessage(text="今天出門的話需要注意：\n")  
+        output_message = TextSendMessage(text="今天出門的話需要注意：\ntest")  
         line_bot_api.reply_message(event.reply_token, output_message)     
     else:  
-        output_message = TextSendMessage(text="請輸入「開始」就可以開始體驗各種功能囉！")  
+        output_message = TemplateSendMessage(
+            alt_text='請輸入「開始」就可以開始體驗各種功能囉！',
+            template=ConfirmTemplate(
+                text='請輸入「開始」就可以開始體驗各種功能囉！',
+                actions=[
+                    PostbackTemplateAction(
+                        label='開始',
+                        text='開始',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='start',
+                        text='start'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, message) 
         line_bot_api.reply_message(event.reply_token, output_message)
 
 import os
